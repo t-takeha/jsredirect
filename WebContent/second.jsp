eda353f2a3834918ce1dc1274e10fb17ed505ce9
t-takeha <t-takeha@earth.email.ne.jp>
2016-07-23 12:51:35 +0900

    sessionからの受け渡しを追加

--- WebContent/second.jsp
+++ WebContent/second.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ page import="java.util.List" %>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -11,6 +11,18 @@
 <body onLoad="autoload();">
 <form name="form1">
 遷移先画面: <%=request.getParameter("windowId") %>
+<BR>
+<%
+	List<String> list = (List<String>) session.getAttribute("mylist");
+	if (list != null) {
+		for (String s : list) {
+%>
+			<p><%=s %></p>
+<%
+		}
+	}
+
+%>
 </form>
 </body>
 </html>
--- src/jsredirect/FirstServlet.java
+++ src/jsredirect/FirstServlet.java
@@ -1,6 +1,8 @@
 package jsredirect;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -30,6 +32,11 @@ public class FirstServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		System.out.println("first");
+		List<String> list = new ArrayList<String>();
+		list.add("abcd");
+		list.add("ABCD");
+
+		request.getSession().setAttribute("mylist", list);
 		RequestDispatcher dispatch = request.getRequestDispatcher("first.jsp");
 		dispatch.forward(request, response);
 	}
